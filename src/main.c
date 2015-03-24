b536a557f01f13c3b4a60ffb219b34848e91b9e2
Robin <Robinhuett@users.noreply.github.com>
2015-03-24 14:26:04 +0100

    Clean up, Optional update times, Changed icon
    - Changed the layout of the code to my likings.
    - Optional update the time every second or minute (Currently only changeable in code)
    - Changed icon

--- src/main.c
+++ src/main.c
@@ -1,18 +1,6 @@
-/*
+//Based on Polar Clock 2.0
 
-  Polar Clock watch (SDK 2.0)
-
-  Thanks to Team Pebble's Segment Six watchface...it was a big help!
-
- */
-
-#include "pebble.h"
-
-/* 
-#define SHOW_TEXT_TIME 1
-#define SHOW_TEXT_DATE 1
-#define ROW_DATE 0
-*/
+#include <pebble.h>
 	
 enum {
         KEY_TIME = 0x0,
@@ -21,19 +9,17 @@ enum {
 		KEY_INVERT = 0x3
 };
 
+//Variables for Settings (Color, etc.)
 static bool SHOW_TEXT_TIME = true;
 static bool SHOW_TEXT_DATE = true;
 static bool ROW_DATE = false;
 static bool INVERT = false;
+static bool UPDATE_EVERY_SECOND = true;
 
-/*
-#define BACKGROUND_COLOR GColorBlack
-#define FOREGROUND_COLOR GColorWhite
-*/
+static GColor BACKGROUND_COLOR;
+static GColor FOREGROUND_COLOR;
 
-static GColor BACKGROUND_COLOR = GColorBlack;
-static GColor FOREGROUND_COLOR = GColorWhite;
-
+//Publics
 Window *window;
 
 Layer *minute_display_layer;
@@ -45,18 +31,41 @@ TextLayer *text_date_layer;
 bool time_layer_exists = false;
 bool date_layer_exists = false;
 
+//Seconds
 const GPathInfo SECOND_SEGMENT_PATH_POINTS = {
-  3,
-  (GPoint []) {
-    {0, 0},
-    {-7, -70}, // 70 = radius + fudge; 7 = 70*tan(6 degrees); 6 degrees per minute;
-    {7,  -70},
-  }
+	3,
+	(GPoint []) {
+		{0, 0},
+		{-7, -70}, // 70 = radius + fudge; 7 = 70*tan(6 degrees); 6 degrees per minute;
+		{7,  -70},
+	}
 };
-
 static GPath *second_segment_path;
+static void second_display_layer_update_callback(Layer *layer, GContext* ctx)
+{
+	time_t now = time(NULL);
+	struct tm *t = localtime(&now);
+
+	unsigned int angle = (t->tm_sec + 1) * 6;
+
+	GRect bounds = layer_get_bounds(layer);
+	GPoint center = grect_center_point(&bounds);
+
+	graphics_context_set_fill_color(ctx, FOREGROUND_COLOR);
+	graphics_fill_circle(ctx, center, 65);
+	graphics_context_set_fill_color(ctx, BACKGROUND_COLOR);
+
+	for(; angle < 355; angle += 6)
+	{
+		gpath_rotate_to(second_segment_path, (TRIG_MAX_ANGLE / 360) * angle);
+		gpath_draw_filled(ctx, second_segment_path);
+	}
+
+	graphics_fill_circle(ctx, center, 60);
+}
 
 
+//Minutes
 const GPathInfo MINUTE_SEGMENT_PATH_POINTS = {
   3,
   (GPoint []) {
@@ -65,53 +74,9 @@ const GPathInfo MINUTE_SEGMENT_PATH_POINTS = {
     {6,  -58},
   }
 };
-
 static GPath *minute_segment_path;
-
-
-const GPathInfo HOUR_SEGMENT_PATH_POINTS = {
-  3,
-  (GPoint []) {
-    {0, 0},
-    {-5, -48}, // 48 = radius + fudge; 5 = 48*tan(6 degrees); 6 degrees per second;
-    {5,  -48},
-  }
-};
-
-static GPath *hour_segment_path;
-
-
-static void second_display_layer_update_callback(Layer *layer, GContext* ctx) {
-
-  time_t now = time(NULL);
-  struct tm *t = localtime(&now);
-
-  unsigned int angle = (t->tm_sec + 1) * 6;
-
-  GRect bounds = layer_get_bounds(layer);
-  GPoint center = grect_center_point(&bounds);
-
-  graphics_context_set_fill_color(ctx, FOREGROUND_COLOR);
-
-  graphics_fill_circle(ctx, center, 65);
-
-  graphics_context_set_fill_color(ctx, BACKGROUND_COLOR);
-
-  for(; angle < 355; angle += 6) {
-
-    gpath_rotate_to(second_segment_path, (TRIG_MAX_ANGLE / 360) * angle);
-
-    gpath_draw_filled(ctx, second_segment_path);
-
-  }
-
-  graphics_fill_circle(ctx, center, 60);
-
-}
-
-
-static void minute_display_layer_update_callback(Layer *layer, GContext* ctx) {
-
+static void minute_display_layer_update_callback(Layer *layer, GContext* ctx)
+{
   time_t now = time(NULL);
   struct tm *t = localtime(&now);
 
@@ -121,26 +86,31 @@ static void minute_display_layer_update_callback(Layer *layer, GContext* ctx) {
   GPoint center = grect_center_point(&bounds);
 
   graphics_context_set_fill_color(ctx, FOREGROUND_COLOR);
-
   graphics_fill_circle(ctx, center, 55);
-
   graphics_context_set_fill_color(ctx, BACKGROUND_COLOR);
 
-  for(; angle < 355; angle += 6) {
-
+  for(; angle < 355; angle += 6)
+	{
     gpath_rotate_to(minute_segment_path, (TRIG_MAX_ANGLE / 360) * angle);
-
     gpath_draw_filled(ctx, minute_segment_path);
-
   }
 
   graphics_fill_circle(ctx, center, 50);
-
 }
 
 
-static void hour_display_layer_update_callback(Layer *layer, GContext* ctx) {
-
+//Hours
+const GPathInfo HOUR_SEGMENT_PATH_POINTS = {
+  3,
+  (GPoint []) {
+    {0, 0},
+    {-5, -48}, // 48 = radius + fudge; 5 = 48*tan(6 degrees); 6 degrees per second;
+    {5,  -48},
+  }
+};
+static GPath *hour_segment_path;
+static void hour_display_layer_update_callback(Layer *layer, GContext* ctx)
+{
   time_t now = time(NULL);
   struct tm *t = localtime(&now);
 
@@ -155,22 +125,18 @@ static void hour_display_layer_update_callback(Layer *layer, GContext* ctx) {
 */
 	
   angle = (( t->tm_hour % 12 ) * 30) + (t->tm_min / 2);
-	  
   angle = angle - (angle % 6);
 
   GRect bounds = layer_get_bounds(layer);
   GPoint center = grect_center_point(&bounds);
 
   graphics_context_set_fill_color(ctx, FOREGROUND_COLOR);
-
   graphics_fill_circle(ctx, center, 45);
-
   graphics_context_set_fill_color(ctx, BACKGROUND_COLOR);
 
-  for(; angle < 355; angle += 6) {
-
+  for(; angle < 355; angle += 6)
+	{
     gpath_rotate_to(hour_segment_path, (TRIG_MAX_ANGLE / 360) * angle);
-
     gpath_draw_filled(ctx, hour_segment_path);
   }
 
@@ -178,10 +144,12 @@ static void hour_display_layer_update_callback(Layer *layer, GContext* ctx) {
 }
 
 
-static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
-  //(void)ctx;
-	
-  layer_mark_dirty(second_display_layer);
+static void handle_tick(struct tm *tick_time, TimeUnits units_changed)
+{
+	if (UPDATE_EVERY_SECOND == true)
+	{
+		layer_mark_dirty(second_display_layer);
+	}
   layer_mark_dirty(minute_display_layer);
   layer_mark_dirty(hour_display_layer);
 
@@ -189,7 +157,6 @@ static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
   {
 	  // Need to be static because it's used by the system later.
 	  static char time_text[] = "00:00";
-	
 	  char *time_format;
 	
 	  if (clock_is_24h_style()) {
@@ -199,7 +166,6 @@ static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
 	  }
 	
 	  strftime(time_text, sizeof(time_text), time_format, tick_time);
-	
 	  text_layer_set_text(text_time_layer, time_text);
   }
 
@@ -214,21 +180,23 @@ static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
 		  //date_text = "Xxx xx";
 	  	  strftime(date_text, sizeof(date_text), "%b %d", tick_time);
 	  }
+		
 	  text_layer_set_text(text_date_layer, date_text);
   }
 }
-
-static void setup_time_date_layers() {
-	
+static void setup_time_date_layers()
+{	
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 	
-  if(time_layer_exists) {
+  if(time_layer_exists)
+	{
 	  text_layer_destroy(text_time_layer);
 	  time_layer_exists = false;	
   }
   
-  if(date_layer_exists) {
+  if(date_layer_exists)
+	{
 	  text_layer_destroy(text_date_layer);
 	  date_layer_exists = false;		
   }
@@ -247,10 +215,11 @@ static void setup_time_date_layers() {
 	  if (SHOW_TEXT_DATE)
 	  {
 	  	layer_set_frame(text_layer_get_layer(text_time_layer), GRect(0, 57, 144, 168-57));
-	  } else {
-	  	layer_set_frame(text_layer_get_layer(text_time_layer), GRect(0, 70, 144, 168-70));
 	  }
-	  
+		else
+		{
+	  	layer_set_frame(text_layer_get_layer(text_time_layer), GRect(0, 70, 144, 168-70));
+	  }  
   }
 
   if (SHOW_TEXT_DATE)
@@ -267,14 +236,16 @@ static void setup_time_date_layers() {
 	  if (SHOW_TEXT_TIME)
 	  {
 		  layer_set_frame(text_layer_get_layer(text_date_layer), GRect(0, 80, 144, 168-80));
-	  } else {
+	  } 
+		else
+		{
 		  layer_set_frame(text_layer_get_layer(text_date_layer), GRect(0, 70, 144, 168-70));
 	  }
-	  
   }
 }
 
- void in_received_handler(DictionaryIterator *received, void *context) {
+ void in_received_handler(DictionaryIterator *received, void *context)
+ {
 	 Tuple *time_tuple = dict_find(received, KEY_TIME);
 	 Tuple *date_tuple = dict_find(received, KEY_DATE);
 	 Tuple *row_tuple = dict_find(received, KEY_ROW);
@@ -305,6 +276,7 @@ static void setup_time_date_layers() {
 		BACKGROUND_COLOR = GColorBlack;
 		FOREGROUND_COLOR = GColorWhite;		 		 
 	 }
+	 
 	 window_set_background_color(window, BACKGROUND_COLOR);
 	 
 	 setup_time_date_layers();
@@ -316,9 +288,8 @@ static void setup_time_date_layers() {
  }
 
 
-static void init(void) {
-  //(void)ctx;
-	
+static void init(void)
+{	
   app_message_register_inbox_received(in_received_handler);
   app_message_register_inbox_dropped(in_dropped_handler);
   app_message_open(64, 0);
@@ -328,11 +299,13 @@ static void init(void) {
   if(persist_exists(KEY_ROW)) ROW_DATE = persist_read_bool(KEY_ROW);
   if(persist_exists(KEY_INVERT)) INVERT = persist_read_bool(KEY_INVERT);
 	
-  if(INVERT) {
+  if(INVERT)
+	{
 	  BACKGROUND_COLOR = GColorWhite;
 	  FOREGROUND_COLOR = GColorBlack;
   }
-  else {
+  else
+	{
 	  BACKGROUND_COLOR = GColorBlack;
 	  FOREGROUND_COLOR = GColorWhite;		 		 
   }
@@ -340,21 +313,22 @@ static void init(void) {
   window = window_create();
   window_set_background_color(window, BACKGROUND_COLOR);
   window_stack_push(window, true);
-
-  // No longer needed for SDK 2.0
-  //resource_init_current_app(&APP_RESOURCES);
 	
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  // Init the layer for the second display
-  second_display_layer = layer_create(bounds);
-  layer_set_update_proc(second_display_layer, second_display_layer_update_callback);
-  layer_add_child(window_layer, second_display_layer);
+	
+	if(UPDATE_EVERY_SECOND == true)
+	{
+		// Init the layer for the second display
+		second_display_layer = layer_create(bounds);
+		layer_set_update_proc(second_display_layer, second_display_layer_update_callback);
+		layer_add_child(window_layer, second_display_layer);
 
-  // Init the second segment path
-  second_segment_path = gpath_create(&SECOND_SEGMENT_PATH_POINTS);
-  gpath_move_to(second_segment_path, grect_center_point(&bounds));
+		// Init the second segment path
+		second_segment_path = gpath_create(&SECOND_SEGMENT_PATH_POINTS);
+		gpath_move_to(second_segment_path, grect_center_point(&bounds));
+	}
 	
   // Init the layer for the minute display
   minute_display_layer = layer_create(bounds);
@@ -376,11 +350,22 @@ static void init(void) {
 
   setup_time_date_layers();
 	
-  tick_timer_service_subscribe(SECOND_UNIT, handle_second_tick);
+	if(UPDATE_EVERY_SECOND == true)
+	{
+  	tick_timer_service_subscribe(SECOND_UNIT, handle_tick);
+	}
+	else
+	{
+		tick_timer_service_subscribe(MINUTE_UNIT, handle_tick);
+	}
 }
 
-static void deinit(void) {
-  gpath_destroy(second_segment_path);
+static void deinit(void)
+{
+	if(UPDATE_EVERY_SECOND == true)
+	{
+  	gpath_destroy(second_segment_path);
+	}
   gpath_destroy(minute_segment_path);
   gpath_destroy(hour_segment_path);
 
@@ -394,7 +379,8 @@ static void deinit(void) {
   if (date_layer_exists) text_layer_destroy(text_date_layer);
 }
 
-int main(void) {
+int main(void)
+{
   init();
   app_event_loop();
   deinit();
